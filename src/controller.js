1811e64cfb915e684078e7ae37ba0ab50f039a31
noeliabahle <noeliabahle1988@gmail.com>
2023-10-24 16:16:00 -0300

    modificaciones agregadas

--- src/controller.js
+++ src/controller.js
@@ -1,24 +1,54 @@
-import {pool} from './database.js';
+import { pool } from './database.js';
 
-class LibroController{
-    async getAll(req, res) {
-        const [result] = await pool.query('SELECT * FROM libros');
-         res.json(result);
+class LibroController {
+  async getAll(req, res) {
+    try {
+      const [result] = await pool.query('SELECT * FROM libros');
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al obtener la lista de libros' });
     }
-    async add(req, res) {
-      const libro = req.body;
-      const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, anio_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.ISBN]);
-      res.json({"Id insertado": result.insertId});
+  }
+
+  async add(req, res) {
+    const libro = req.body;
+    try {
+      const [result] = await pool.query(
+        `INSERT INTO Libros(nombre, autor, categoria, anio_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`,
+        [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.ISBN]
+      );
+      res.json({ "Id insertado": result.insertId });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al insertar el libro' });
     }
-    async delete(req, res){
-     const libro = req.body;
-     const [result] = await pool.query(`DELETE FROM Libros WHERE id=(?)`, [libro.id]);
-     res.json({"registros eliminados": result.affectedRows});
+  }
+
+  async delete(req, res) {
+    const libro = req.body;
+    try {
+      const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [libro.ISBN]);
+      res.json({ "registros eliminados": result.affectedRows });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al eliminar el libro' });
     }
-    async update(req, res){
-     const libro = req.body;
-     const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), anio_publicacion=(?), ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.ISBN, libro.id]);
-     res.json({"registros actualizados": result.changedRows});
+  }
+
+  async update(req, res) {
+    const libro = req.body;
+    try {
+      const [result] = await pool.query(
+        `UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), anio_publicacion=(?), ISBN=(?) WHERE id=(?)`,
+        [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.ISBN, libro.id]
+      );
+      res.json({ "registros actualizados": result.changedRows });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al actualizar el libro' });
     }
+  }
 }
+
 export const libro = new LibroController();
